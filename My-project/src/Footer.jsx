859d7d41b333bd4aa8b35f2cad42e9850e1af524
Thejandeera <thejan1010@gmail.com>
2024-06-17 16:48:29 +0530

    Second commit

---
+++ My-project/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer (){
+    return (
+        
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Thejandeera Sandeepana</p>
+        </footer>
+    );
+}
+
+export default Footer
